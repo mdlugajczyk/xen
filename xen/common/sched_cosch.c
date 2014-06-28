750ecd7ca0d785d190bba9c669fe303f0cf316ca
Marcin Długajczyk <dlugajczykmarcin@gmail.com>
2014-06-28 14:44:22 +0100

    Fix runq for multiple cpus.

--- xen/common/sched_cosch.c
+++ xen/common/sched_cosch.c
@@ -21,6 +21,7 @@ struct cosch_cpu_private {
   struct cosch_vcpu_private *vcpus[256];
   unsigned int current_vcpu;
   unsigned int last_vcpu;
+  spinlock_t lock;
 };
 
 #define COSCH_PRIV(_ops) \
@@ -49,22 +50,6 @@ cosch_init(struct scheduler *ops)
 static void
 cosch_insert_vcpu(const struct scheduler *ops, struct vcpu *v)
 {
-    unsigned long flags;
-    struct cosch_cpu_private *cpu_priv = COSCH_PCPU(v->processor);
-    struct cosch_private *prv = COSCH_PRIV(ops);
-    MD_PRINT();
-      
-    if (is_idle_vcpu(v) )
-    {
-	return;
-    }
-
-    spin_lock_irqsave(&prv->lock, flags);
-    
-    cpu_priv->vcpus[cpu_priv->last_vcpu] = COSCH_VCPU_PRIV(v);
-    cpu_priv->last_vcpu++;
-
-    spin_unlock_irqrestore(&prv->lock, flags);
 }
 
 static void
@@ -72,12 +57,10 @@ cosch_remove_vcpu(const struct scheduler *ops, struct vcpu *v)
 {
     unsigned int i;
     unsigned long flags;
-    const int cpu = smp_processor_id();
-    struct cosch_cpu_private *cpu_priv = COSCH_PCPU(cpu);
-    struct cosch_private *prv = COSCH_PRIV(ops);
+    struct cosch_cpu_private *cpu_priv = COSCH_PCPU(v->processor);
     MD_PRINT();
 
-    spin_lock_irqsave(&prv->lock, flags);
+    spin_lock_irqsave(&cpu_priv->lock, flags);
     
     for (i = 0; i < cpu_priv->last_vcpu; i++)
     {
@@ -88,7 +71,7 @@ cosch_remove_vcpu(const struct scheduler *ops, struct vcpu *v)
         }
     }
 
-     spin_unlock_irqrestore(&prv->lock, flags);
+    spin_unlock_irqrestore(&cpu_priv->lock, flags);
     MD_PRINT();
 }
 
@@ -102,11 +85,10 @@ cosch_do_schedule(const struct scheduler *ops, s_time_t now,
     struct task_slice ret;
     struct cosch_vcpu_private *next = NULL;
     unsigned long flags;
-    struct cosch_private *prv = COSCH_PRIV(ops);
     struct vcpu *curr = per_cpu(schedule_data, cpu).curr;
     MD_PRINT();
 
-    spin_lock_irqsave(&prv->lock, flags);
+    spin_lock_irqsave(&cpu_priv->lock, flags);
     ret.migrated = 0;
     ret.time = DEFAULT_TIMESLICE;
     ret.task = NULL;
@@ -133,7 +115,7 @@ cosch_do_schedule(const struct scheduler *ops, s_time_t now,
 	ret.task = idle_vcpu[cpu];
     }
 
-    spin_unlock_irqrestore(&prv->lock, flags);
+    spin_unlock_irqrestore(&cpu_priv->lock, flags);
     /* printk("new vcpu: %d %d %d next index: %d state: %d for time: %ld\n", ret.task->vcpu_id, ret.task->processor, */
     /*        ret.task->domain->domain_id, cpu_priv->current_vcpu, ret.task->runstate.state, ret.time); */
     /* printk("Total number of vcpus: %d current_cpu: %d\n", cpu_priv->last_vcpu,cpu); */
@@ -173,10 +155,10 @@ cosched_alloc_pdata(const struct scheduler *ops, int cpu)
     
     if (pcpu == NULL)
       return pcpu;
-
+    printk("PCPU for cpu: %d\n", cpu);
     pcpu->current_vcpu = 0;
     pcpu->last_vcpu = 0;
-
+    spin_lock_init(&pcpu->lock);
     return pcpu;
 }
 
@@ -204,13 +186,32 @@ cosched_free_domdata(const struct scheduler *ops, void *data)
 
 static void cosch_wake(const struct scheduler *ops, struct vcpu *v)
 {
+    int vcpu_added = 0;
+    unsigned long flags;
+    unsigned int i;
+    struct cosch_cpu_private *cpu_priv = COSCH_PCPU(v->processor);
     MD_PRINT();
 
     if ( is_idle_vcpu(v) )
     {
         return;
     }
+
+    spin_lock_irqsave(&cpu_priv->lock, flags);
     COSCH_VCPU_PRIV(v)->awake = 1;
+
+    for (i = 0; i < cpu_priv->last_vcpu; i++)
+	if ( cpu_priv->vcpus[i] == COSCH_VCPU_PRIV(v))
+	    vcpu_added = 1;
+
+    if (vcpu_added == 0)
+    {
+	cpu_priv->vcpus[cpu_priv->last_vcpu] = COSCH_VCPU_PRIV(v);
+	cpu_priv->last_vcpu++;
+    }
+
+    spin_unlock_irqrestore(&cpu_priv->lock, flags);
+    
     if (is_idle_vcpu(per_cpu(schedule_data, v->processor).curr))
     {
 	cpu_raise_softirq(v->processor, SCHEDULE_SOFTIRQ);
